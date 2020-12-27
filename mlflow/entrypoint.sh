b90137648d7d8c0751eed17b0a4ce5e637400f8a
William Hearn <sylus1984@gmail.com>
2020-12-27 15:12:48 -0500

    feat(mlflow): Bump up to v1.12.1

--- mlflow/entrypoint.sh
+++ mlflow/entrypoint.sh
@@ -3,6 +3,8 @@
 export MLFLOW_SERVER_FILE_STORE="${MLFLOW_SERVER_FILE_STORE:-./mlruns}"
 export MLFLOW_SERVER_DEFAULT_ARTIFACT_ROOT="${MLFLOW_SERVER_DEFAULT_ARTIFACT_ROOT:-./mlruns}"
 
+mlflow db upgrade $MLFLOW_SERVER_FILE_STORE
+
 mlflow server \
     --backend-store-uri "$MLFLOW_SERVER_FILE_STORE" \
     --default-artifact-root "$MLFLOW_SERVER_DEFAULT_ARTIFACT_ROOT" \
